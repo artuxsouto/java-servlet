67a96ce07932b9ac9b22094c7450e65038beccd0
Artur Siqueira <artursiqueirasouto@hotmail.com>
2022-06-21 14:14:49 -0300

    Quando o Dispatcher ñ atende

--- src/main/java/br/com/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/gerenciador/servlet/NovaEmpresaServlet.java
@@ -35,7 +35,7 @@ public class NovaEmpresaServlet extends HttpServlet {
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 		
-		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+		RequestDispatcher rd = request.getRequestDispatcher("/listEmpresas");
 		request.setAttribute("empresa", empresa.getNome());
 		rd.forward(request, response);
 	}
