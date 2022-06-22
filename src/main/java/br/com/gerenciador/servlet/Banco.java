742e248f5bb57f7733799f345c15a59ed2f5ae8b
Artur Siqueira <artursiqueirasouto@hotmail.com>
2022-06-22 10:05:30 -0300

    Removendo empresa

--- src/main/java/br/com/gerenciador/servlet/Banco.java
+++ src/main/java/br/com/gerenciador/servlet/Banco.java
@@ -1,23 +1,41 @@
 package br.com.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 	
 	private static List<Empresa> list = new ArrayList<>();
+	private static Integer chaveSequencial = 1;
 
 	static {
 		Empresa empresa1 = new Empresa("Alura");
+		empresa1.setId(chaveSequencial++);
 		Empresa empresa2 = new Empresa("Caelum");
+		empresa2.setId(chaveSequencial++);
 		list.add(empresa1);
 		list.add(empresa2);
 	}
 	public void adiciona(Empresa e) {
+		e.setId(Banco.chaveSequencial++);
 		list.add(e);
 	}
 	
 	public List<Empresa> getEmpresas() {
 		return Banco.list;
+	}
+
+	public void removeEmpresa(Integer id) {
+		
+		Iterator<Empresa> it = list.iterator();
+		
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			
+			if(emp.getId() == id) {
+				it.remove();
+			}
+		}	
 	}
 }
