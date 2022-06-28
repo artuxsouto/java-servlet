a0868e9180d5c6b9b2db619cb234df0e3e0cecf4
Artur Siqueira <artursiqueirasouto@hotmail.com>
2022-06-28 20:06:24 -0300

    criando login e verificando

--- src/main/java/br/com/gerenciador/modelo/Banco.java
+++ src/main/java/br/com/gerenciador/modelo/Banco.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> list = new ArrayList<>();
+	private static List<Usuario> listaUsuarios= new ArrayList<>();
 	private static Integer chaveSequencial = 1;
 
 	static {
@@ -16,6 +17,16 @@ public class Banco {
 		empresa2.setId(chaveSequencial++);
 		list.add(empresa1);
 		list.add(empresa2);
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("artur");
+		u1.setSenha("12345");
+		Usuario u2 = new Usuario();
+		u2.setLogin("nairana");
+		u2.setSenha("12345");
+		
+		listaUsuarios.add(u1);
+		listaUsuarios.add(u2);
 	}
 	public void adiciona(Empresa e) {
 		e.setId(Banco.chaveSequencial++);
@@ -45,6 +56,15 @@ public class Banco {
 				return empresa;
 			}
 			
+		}
+		return null;
+	}
+
+	public Usuario existeUsuario(String login, String senha) {
+		for (Usuario u : listaUsuarios) {
+			if (u.ehIgual(login, senha)) {
+				return u;
+			}
 		}
 		return null;
 	}
