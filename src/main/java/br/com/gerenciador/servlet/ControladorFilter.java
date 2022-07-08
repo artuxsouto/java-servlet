5df3f3448f6e793cdba31638811a82c45b8da992
Artur Siqueira <artursiqueirasouto@hotmail.com>
2022-07-07 22:34:08 -0300

    Organizando os filtros

--- src/main/java/br/com/gerenciador/servlet/ControladorFilter.java
+++ src/main/java/br/com/gerenciador/servlet/ControladorFilter.java
@@ -2,34 +2,32 @@ package br.com.gerenciador.servlet;
 
 import java.io.IOException;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import br.com.gerenciador.acao.Acao;
 
-//@WebServlet("/entrada")
-public class UnicaEntradaServlet extends HttpServlet {
+//@WebFilter("/entrada")
+public class ControladorFilter extends HttpFilter implements Filter {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+		
+		System.out.println("ControladorFilter");
+		
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
 		
 		String paramAcao = request.getParameter("acao");
-
-////		HttpSession sessao = request.getSession();
-////		boolean usuarioNaoLogado = sessao.getAttribute("usuarioLogado") == null;
-////		boolean acaoProtegida = !(paramAcao.equals("Login") || paramAcao.equals("LoginForm"));
-////		
-////		if (acaoProtegida && usuarioNaoLogado) {
-////			response.sendRedirect("entrada?acao=LoginForm");
-////			return;
-////		}
-
 		String nomeClass = "br.com.gerenciador.acao." + paramAcao;
 
 		String nome;
@@ -49,4 +47,5 @@ public class UnicaEntradaServlet extends HttpServlet {
 			response.sendRedirect(tipoEEndereco[1]);
 		}
 	}
+
 }
