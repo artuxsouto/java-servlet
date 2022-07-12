827d09326d82a88539235564996e563e535d2309
Artur Siqueira <artursiqueirasouto@hotmail.com>
2022-07-12 17:23:26 -0300

    Servlet Container - Deploy pra jetty

--- src/main/java/br/com/gerenciador/servlet/ControladorFilter.java
+++ src/main/java/br/com/gerenciador/servlet/ControladorFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -17,8 +18,13 @@ import javax.servlet.http.HttpSession;
 import br.com.gerenciador.acao.Acao;
 
 //@WebFilter("/entrada")
-public class ControladorFilter extends HttpFilter implements Filter {
-	private static final long serialVersionUID = 1L;
+public class ControladorFilter implements Filter {
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {}
+	
+	@Override
+	public void destroy() {}
 
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 		
